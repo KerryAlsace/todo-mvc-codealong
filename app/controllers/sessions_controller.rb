4ed83344e3f9abf55f2915dad8caecf3deba74bf
KerryAlsace <kerrydougherty@gmail.com>
2016-09-14 16:23:14 -0400

    add logout button to index

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # raise cookies.inspect --> let's you check out the cookies
     user = User.create(email: params[:email])
     session[:user_id] = user.id
 
     redirect_to root_path
+  end
+
+  def destroy
+    reset_session
   end
 
 end

9b484f3096f75fb755d078b161986de2a160c181
KerryAlsace <kerrydougherty@gmail.com>
2016-09-14 16:29:56 -0400

    add current_user methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,16 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    session[:user_id]
+    !!current_user
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # @current_user ||= this part is to make sure you don't have to query your database every time, that you save the current user and can reuse it while it's still the same user
+  end
+
+  # methods built in controllers do not permeate to action view, must add this as a helper method to use in view
+
+  helper_method :current_user
 
 end

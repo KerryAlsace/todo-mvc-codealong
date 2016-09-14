592bb4c86b2d75226296b2b74d2ea44f9d7c8d0a
KerryAlsace <kerrydougherty@gmail.com>
2016-09-14 15:20:31 -0400

    add create action to users_controller to complete signup action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,12 @@ class UsersController < ApplicationController
   end
 
   def create
+    # raise params.inspect -- always do first to double check that the form is correctly submitting data to the controller
+    @user = User.new
+    @user.email = params[:user][:email]
+    @user.save
+
+    redirect_to root_path
   end
 
 end

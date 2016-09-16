623ce6efffa6945cbca886dc3a6befa906373ddc
KerryAlsace <kerrydougherty@gmail.com>
2016-09-16 14:37:13 -0400

    finish oauth login flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if auth_hash = request.env["omniauth.auth"] # they logged in via OAuth
-      raise auth_hash.inspect
+    if auth_hash = request.env["omniauth.auth"]["info"] # they logged in via OAuth
+      if user = User.find_by(email: auth_hash["email"]) # they've logged into my app before using oath
+        session[:user_id] = user.id
+        redirect_to root_path
+      else # they're a new user logging in with oath
+        user = User.create(email: auth_hash["email"], password: SecureRandom.hex)
+        session[:user_id] = user.id
+        redirect_to root_path
+      end
     else # they logged in normally
       user = User.find_by(email: params[:email])
       if user && user.authenticate(params[:password])

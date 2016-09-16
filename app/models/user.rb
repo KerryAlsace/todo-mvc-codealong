55998f3eff88c4dff2a3992af95de9df698b0e4c
KerryAlsace <kerrydougherty@gmail.com>
2016-09-16 15:03:32 -0400

    refractor

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,8 @@ class User < ApplicationRecord
   validates_uniqueness_of :email, message: "That email is taken, please use another"
 
   def self.find_or_create_by_omniauth(auth_hash)
-    if user = User.find_by(email: auth_hash["email"])
-      return user
-    else
-      user = User.create(email: auth_hash["email"], password: SecureRandom.hex)
+    self.where(email: auth_hash["email"]).first_or_create do |user|
+      user.password = SecureRandom.hex
     end
   end
 

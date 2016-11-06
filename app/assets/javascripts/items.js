abee401bfdc974c8489eca5956d5a8a92ebeeb5c
KerryAlsace <kerrydougherty@gmail.com>
2016-11-06 16:55:38 -0500

    refractor

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -30,16 +30,20 @@ Item.formSubmit = function(e) {
 	.error(Item.error)
 };
 
-$(function() {
+Item.formSubmitListener = function() {
+	$("form#new_item").on("submit", Item.formSubmit);
+};
+
+Item.ready = function() {
 	Item.templateSource = $("#item-template").html();
 	Item.template = Handlebars.compile(Item.templateSource);
-});
+	Item.formSubmitListener();
+};
 
 Item.prototype.renderLI = function() {
 	return Item.template(this)
 };
 
-
 $(function() {
-	$("form#new_item").on("submit", Item.formSubmit);
+	Item.ready();
 });

1e004a4c8ccbee878490bfb629cd3d283bc6fdb8
KerryAlsace <kerrydougherty@gmail.com>
2016-11-06 17:01:20 -0500

    start building out delete with ajax

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -30,6 +30,13 @@ Item.formSubmit = function(e) {
 	.error(Item.error)
 };
 
+Item.destroyListener = function() {
+	$("input.destroy").on("click", function(e) {
+		e.preventDefault();
+		console.log("Hijacked the click");
+	});
+};
+
 Item.formSubmitListener = function() {
 	$("form#new_item").on("submit", Item.formSubmit);
 };
@@ -38,6 +45,7 @@ Item.ready = function() {
 	Item.templateSource = $("#item-template").html();
 	Item.template = Handlebars.compile(Item.templateSource);
 	Item.formSubmitListener();
+	Item.destroyListener();
 };
 
 Item.prototype.renderLI = function() {
